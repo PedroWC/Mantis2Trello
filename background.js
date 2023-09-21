c6a1ed442d8d350a2fd8dbb52d36416e1a9bb4f0
Pedro Carvalhaes <pedrohwc1998@gmail.com>
2023-09-21 00:24:45 -0400

    comentarios

--- background.js
+++ background.js
@@ -1,46 +1,56 @@
+// Este ouvinte é chamado quando a extensão é instalada ou atualizada
 chrome.runtime.onInstalled.addListener(function () {
-  // Replace all rules ...
+  // Remove todas as regras existentes para a extensão
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    // With a new rule ...
+    // Adiciona novas regras para a extensão
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        // That fires when a page's URL contains a 'mantis/view.php' ...
+        // Condições para ativar a extensão
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
+            // Padrões de URL que ativam a extensão
             pageUrl: { urlMatches: 'mantis/view.php || bugs/view.php || view.php' },
           })
         ],
-        // And shows the extension's page action.
+        // Ação a ser tomada quando as condições são atendidas
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }
     ]);
   });
-
+  // Abre a página de opções da extensão após a instalação
   chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
 });
 
+// Função executada quando o ícone da extensão é clicado
 function sendClickMessage() {
+  // Obtém a aba ativa atual
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentTab = tabs[0].id;
-    chrome.pageAction.setIcon({
+    // Muda o ícone da extensão para um ícone de "carregando"
+    chrome.action.setIcon({
       path: "Mantis2TrelloLoadingIcon16.png",
       tabId: currentTab
     });
+    // Envia uma mensagem para o conteúdo da aba atual
     chrome.tabs.sendMessage(currentTab, "");
   });
 };
 
+// Callback para mensagens recebidas da extensão
 function onMessageCallback(request, sender, sendResponse) {
+  // Obtém a aba ativa atual
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentTab = tabs[0].id;
-    chrome.pageAction.setIcon({
+    // Muda o ícone da extensão de volta para o ícone padrão
+    chrome.action.setIcon({
       path: "Mantis2TrelloIcon16.png",
-      tabId: currentTab
+      tabId: currentTab  // O tabId ainda pode ser usado se você quiser que o ícone seja atualizado apenas para uma aba específica
     });
-
-  });
+  });  
 };
 
-chrome.pageAction.onClicked.addListener(sendClickMessage);
+// Adiciona um ouvinte para o evento de clique no ícone da extensão
+chrome.action.onClicked.addListener(sendClickMessage);
 
+// Adiciona um ouvinte para mensagens recebidas da extensão
 chrome.runtime.onMessage.addListener(onMessageCallback);
